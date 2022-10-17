04db1b6af7ddf5fb23548fd863b319d4941fcaf8
Your Name <you@example.com>
2022-10-17 15:14:55 +0800

    123

--- src/main/java/com/you/community/controller/AuthorizeController.java
+++ src/main/java/com/you/community/controller/AuthorizeController.java
@@ -1,8 +1,10 @@
 package com.you.community.controller;
 
 import com.you.community.dto.AccessTokenDTO;
+import com.you.community.dto.GithubUser;
 import com.you.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,16 +14,25 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String clientUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
-        accessTokenDTO.setClient_id("35ba2c7a38a011ad3398");
+        accessTokenDTO.setRedirect_uri(clientUri);
+        accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_secret("36842d69dfcd706d1a7e085ac4f4ff26b7656c76");
-        githubProvider.getAccessToken(accessTokenDTO);
+        accessTokenDTO.setClient_secret(clientSecret);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 
--- src/main/java/com/you/community/provider/GithubProvider.java
+++ src/main/java/com/you/community/provider/GithubProvider.java
@@ -1,6 +1,8 @@
 package com.you.community.provider;
 
+import com.alibaba.fastjson.JSON;
 import com.you.community.dto.AccessTokenDTO;
+import com.you.community.dto.GithubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
@@ -9,21 +11,60 @@ import java.io.IOException;
 @Component
 public class GithubProvider {
     public String getAccessToken(AccessTokenDTO accessTokenDTO){
-        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
 
         OkHttpClient client = new OkHttpClient();
 
-        RequestBody body = RequestBody.create(json, JSON);
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
         Request request = new Request.Builder()
-                .url(url)
+                .url("https://github.com.login/oauth/access_token")
                 .post(body)
                 .build();
         try (Response response = client.newCall(request).execute()) {
             String string = response.body().string();
-            System.out.println(string);
-            return string;
+            String[] split = string.split("&");
+            String tokenstr = split[0];
+            String token = tokenstr.split("=")[1];
+            return token;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
         } catch (IOException e) {
         }
         return null;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
